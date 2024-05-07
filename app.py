16d98e70483c204cbf429569c7914089b4583266
Barry Weiss <bweiss@pegasusnet.net>
2024-05-07 08:56:04 -0400

    Improve weather command performance and add typing indication

--- app.py
+++ app.py
@@ -21,33 +21,29 @@ class YumiClient(discord.Client):
         if message.author == self.user:
             return
 
-        if message.content.startswith("!chat"):
-            query = message.content.split("!chat ")[1]
-            response = basic_convo.ainvoke({"query": query})
-            await message.channel.send(await response)
-
-        if message.content.startswith("!weather"):
-            city = message.content.split("!weather ")[1]
-            weather_data = get_weather(city)
-            weather_description = weather_data["weather"][0]["description"]
-            temperature = weather_data["main"]["temp"]
-            await message.channel.send(
-                dedent(
-                    f"""\
-                    The weather in {city} is {weather_description} with
-                    a temperature of {temperature:.2f}°C"""
+        elif message.content.startswith("!weather"):
+            async with message.channel.typing():
+                city = message.content.split("!weather ")[1]
+                weather_data = get_weather(city)
+                weather_description = weather_data["weather"][0]["description"]
+                temperature = weather_data["main"]["temp"]
+                await message.channel.send(
+                    dedent(
+                        f"""\
+                        The weather in {city} is {weather_description} with
+                        a temperature of {temperature:.2f}°C"""
+                    )
                 )
-            )
 
-        if message.content.startswith("!ping"):
-            await message.channel.send("pong")
         else:
-            response = basic_convo.ainvoke({"query": message.content})
-            await message.channel.send(await response)
+            async with message.channel.typing():
+                response = basic_convo.ainvoke({"query": message.content})
+                await message.channel.send(await response)
 
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.typing = True
 
 client = YumiClient(intents=intents)
 client.run(BOT_TOKEN)
