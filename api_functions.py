96b011ad0e8fb1e0c96cabb8ca949b293679cbfa
Barry Weiss <bweiss@pegasusnet.net>
2024-05-19 18:57:30 -0400

    Add Models Directory for Pydantic Models

--- api_functions.py
+++ api_functions.py
@@ -10,7 +10,7 @@ WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
 weather_api_url = "https://api.openweathermap.org/data/2.5/weather"
 
 
-def get_weather(city: str, units: str = "imperial") -> dict:
+def get_current_weather(city: str, units: str = "imperial") -> dict:
     if units not in ["imperial", "metric", "standard"]:
         raise ValueError("units must be either 'imperial' or 'metric'")
     url = f"{weather_api_url}?q={city}&units={units}&appid={WEATHER_API_KEY}"
