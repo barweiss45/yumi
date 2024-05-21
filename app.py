e12bbc2fd2a7cbb887b4e58fbda4681db8095e8d
Barry Weiss <bweiss@pegasusnet.net>
2024-05-21 08:36:26 -0400

    Refactor message handling and conversation logic in YumiClient class

--- app.py
+++ app.py
@@ -46,41 +46,6 @@ class YumiClient(discord.Client):
         for i in range(0, len(response), 2000):
             return await message.channel.send(response[i : i + 2000])  # noqa: E203
 
-        # elif message.content.startswith("!weather "):
-        #     async with message.channel.typing():
-        #         yumi_logger.debug("--> Starting Weather Conversation <--")
-        #         response = await get_weather(
-        #             query=message.content.split("!weather ")[1],
-        #             config={"configurable": {"session_id": "abc123"}})
-
-        # elif message.content.startswith("!rag "):
-        #     yumi_logger.debug("--> Starting RAG Conversation <--")
-        #     async with message.channel.typing():
-        #         response = await basic_rag_conversation(
-        #             query=message.content.split("!rag ")[1],
-        #             config={"configurable": {"session_id": "abc123"}},
-        #         )
-
-        # elif len(message.attachments) > 0:
-        #     yumi_logger.debug("--> Uploading Attachments to Pinecone <--")
-        #     response = await load_pdfs_to_pinecone(
-        #         attachments=message.attachments, index_name="yumi-test-1"
-        #     )  # Future: message.content
-        #     return await message.channel.send(response)
-
-        # else:
-        #     async with message.channel.typing():
-        #         yumi_logger.debug("--> Starting Basic Conversation <--")
-        #         response = await baisc_conversation().ainvoke(
-        #             {"query": message.content},
-        #             config={"configurable": {"session_id": "abc123"}},
-        #         )
-
-        # # Send the response in chunks of 2000 characters due character limit
-        # i = 0
-        # for i in range(0, len(response), 2000):
-        #     await message.channel.send(response[i : i + 2000])  # noqa: E203
-
 
 intents = discord.Intents.default()
 intents.message_content = True
